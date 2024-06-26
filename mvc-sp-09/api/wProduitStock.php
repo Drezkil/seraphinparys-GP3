e41dd006c356f8b1c55b8e9dc897ef66ec330b2d
Victor Chaumier <chaumiervictor@gmail.com>
2024-06-26 18:09:02 +0200

    mise en forme du JSON

--- mvc-sp-09/api/wProduitStock.php
+++ mvc-sp-09/api/wProduitStock.php
@@ -13,7 +13,7 @@ try {
 
         $cnx = new PDO('mysql:host=' . SERVEUR . ';dbname=' . BASE, NOM, PASSE, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-        $sql = "SELECT * FROM produit WHERE produits.stock < 50";
+        $sql = "SELECT * FROM produit WHERE stock < 50";
 
         $idRequete = $cnx->query($sql);
 
@@ -23,7 +23,7 @@ try {
 
         while($row = $idRequete->fetch(PDO::FETCH_ASSOC)){
 
-            echo $row['reference'] . " ; " . $row['designation'] . " ; " . $row['quantite'] . " ; " . " ; " . $row['descriptif'] . $row['prix_unitaire_HT'] . " ; " . $row['stock'] . " separateur ";
+            echo $row['reference'] . " ; " . $row['designation'] . " ; " . $row['quantite'] . " ; " . $row['descriptif'] . " ; " . $row['prix_unitaire_HT'] . " ; " . $row['stock'];
         }
 
 
